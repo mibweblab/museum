d7d6f2bff0bdbc577ddcfd18f3a32ee14a099ad9
Hillary T Tamirepi <hillaryttamirepi@C02GM0U0MD6M.tld>
2022-01-14 02:06:14 -0500

    added character movements

--- client/src/components/modules/World.js
+++ client/src/components/modules/World.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useLayoutEffect, useState } from "react";
+import React, { Suspense, useLayoutEffect, useState, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { HexColorPicker } from "react-colorful";
 import { proxy, useSnapshot } from "valtio";
@@ -91,69 +91,68 @@ function ToggleColor() {
   );
 }
 
-function Ploid() {
+
+const World = () => {
+
+  const control = useRef();
+  const camera = useRef()
   const snap = useSnapshot(state);
+
   return (
-    <Model
-      clothes={snap.items.clothes}
-      body={snap.items.body}
-      chest={snap.items.chest}
-      eyes={snap.items.eyes}
-      scale={0.5}
-      position={[0, 0, 0]}
-    />
-  );
-}
-
-export default class World extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="World">
-        <ToggleColor />
-        <Canvas dpr={[1, 2]} shadows>
-          <ambientLight intensity={0.5} />
-          <color attach="background" args={["#d25578"]} />
-          <fog attach="fog" args={["#101010", 10, 20]} />
-          <Suspense fallback={null}>
-            <Environment preset="city" />
-            <group position={[0, -0.5, 0]}>
-              <Ploid />
-              <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-                <planeGeometry args={[150, 150]} />
-                <MeshReflectorMaterial
-                  blur={[400, 100]}
-                  resolution={1024}
-                  mixBlur={1}
-                  opacity={4}
-                  depthScale={1.1}
-                  minDepthThreshold={0.4}
-                  maxDepthThreshold={1.25}
-                  roughness={1}
-                />
-              </mesh>
-              <mesh receiveShadow rotation-x={-Math.PI / 2} position={[0, 0.001, 0]}>
-                <planeGeometry args={[10, 10]} />
-                <shadowMaterial transparent color="black" opacity={0.4} />
-              </mesh>
-            </group>
-            <OrbitControls
-              makeDefault
-              autoRotate
-              autoRotateSpeed={0.3}
-              maxPolarAngle={Math.PI / 2.3}
-              minPolarAngle={Math.PI / 2.3}
-              enableZoom={false}
-              enablePan={false}
+    <div className="World">
+      <ToggleColor />
+      <Canvas dpr={[1, 2]} shadows>
+        <ambientLight intensity={0.5} />
+        <color attach="background" args={["#d25578"]} />
+        <fog attach="fog" args={["#101010", 10, 20]} />
+        <Suspense fallback={null}>
+          <Environment preset="city" />
+          <group position={[0, -0.5, 0]}>
+            <Model
+              clothes={snap.items.clothes}
+              body={snap.items.body}
+              chest={snap.items.chest}
+              eyes={snap.items.eyes}
+              scale={0.5}
+              position={[0, 0, 0]}
+              controls={control}
+              camera = {camera}
             />
-            <PerspectiveCamera makeDefault fov={65} position={[-4, 0, 4]}>
-              {/* <spotLight
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+              <planeGeometry args={[12, 12]} />
+              <MeshReflectorMaterial
+                blur={[400, 100]}
+                resolution={1024}
+                mixBlur={1}
+                opacity={4}
+                depthScale={1.1}
+                minDepthThreshold={0.4}
+                maxDepthThreshold={1.25}
+                roughness={1}
+              />
+            </mesh>
+            <mesh receiveShadow rotation-x={-Math.PI / 2} position={[0, 0.001, 0]}>
+              <planeGeometry args={[10, 10]} />
+              <shadowMaterial transparent color="black" opacity={0.4} />
+            </mesh>
+          </group>
+
+          <OrbitControls
+            makeDefault
+            // autoRotate
+            // autoRotateSpeed={0.3}
+            maxPolarAngle={Math.PI / 2 - 0.05}
+            // minPolarAngle={Math.PI / 2.3}
+            // enableZoom={true}
+            enablePan={false}
+            minDistance={5}
+            maxDistance={15}
+            enableDamping={true}
+            ref={control}
+          />
+
+          <PerspectiveCamera ref={camera} fov={45} position={[0, 5, 5]} aspect={window.innerWidth/window.innerHeight}>
+            {/* <spotLight
                 position={[-4, 10, -10]}
                 angle={0.15}
                 penumbra={1}
@@ -161,10 +160,10 @@ export default class World extends React.Component {
                 castShadow
                 shadow-mapSize={[2048, 2048]}
               /> */}
-            </PerspectiveCamera>
-          </Suspense>
-        </Canvas>
-      </div>
-    );
-  }
-}
+          </PerspectiveCamera>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+export default World;
--- client/src/components/three-utils.js
+++ client/src/components/three-utils.js
@@ -1,15 +1,25 @@
-import { AnimationMixer } from 'three';
-function setupModel(data) {
-    const model = data.scene.children[0];
-    const clip = data.animations[0];
-  
-    const mixer = new AnimationMixer(model);
-    const action = mixer.clipAction(clip);
-    action.play();
-  
-    model.tick = (delta) => mixer.update(delta);
-  
-    return model;
-  }
+import { useEffect, useRef } from 'react'
 
-  export {setupModel}
+export function useKeyPress(target, event) {
+  useEffect(() => {
+    const downHandler = ({ key }) => target.indexOf(key) !== -1 && event(true)
+    const upHandler = ({ key }) => target.indexOf(key) !== -1 && event(false)
+    window.addEventListener('keydown', downHandler)
+    window.addEventListener('keyup', upHandler)
+    return () => {
+      window.removeEventListener('keydown', downHandler)
+      window.removeEventListener('keyup', upHandler)
+    }
+  }, [])
+}
+
+export function useControls() {
+  const keys = useRef({ forward: false, backward: false, left: false, right: false, brake: false, reset: false })
+  useKeyPress(['ArrowUp', 'w'], (pressed) => (keys.current.forward = pressed))
+  useKeyPress(['ArrowDown', 's'], (pressed) => (keys.current.backward = pressed))
+  useKeyPress(['ArrowLeft', 'a'], (pressed) => (keys.current.left = pressed))
+  useKeyPress(['ArrowRight', 'd'], (pressed) => (keys.current.right = pressed))
+  useKeyPress([' '], (pressed) => (keys.current.brake = pressed))
+  useKeyPress(['r'], (pressed) => (keys.current.reset = pressed))
+  return keys
+}
