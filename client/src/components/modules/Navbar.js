9034842e3d5365b79a38ce8f4d357f6ed47d9be2
Hillary T Tamirepi <hillaryttamirepi@C02GM0U0MD6M.tld>
2022-01-18 13:28:33 -0500

    added auth to backend

--- client/src/components/modules/Navbar.js
+++ client/src/components/modules/Navbar.js
@@ -8,6 +8,7 @@ import "./NavBar.css";
 // This identifies your web application to Google's authentication service
 // const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
 const GOOGLE_CLIENT_ID = "486452721555-mv97gl89cqbdemntlnbugl44c72iphuv.apps.googleusercontent.com"
+// 486452721555-mv97gl89cqbdemntlnbugl44c72iphuv.apps.googleusercontent.com
 
 class NavBar extends Component {
   constructor(props) {
