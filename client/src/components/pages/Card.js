ad657e985b0525ca8456292498a4ec9e379e9b41
Julia Pearl <julia@ipearl.net>
2022-01-26 10:16:42 -0500

    public and private icons mismatched

--- client/src/components/pages/Card.js
+++ client/src/components/pages/Card.js
@@ -77,7 +77,7 @@ const MuseumCard = ({
           </div>
       
           <div className="Card-icons">
-            {isPrivate && 
+            {!isPrivate && 
             <span className="Card-status">
               <img
               src="https://img.icons8.com/ios/50/000000/visible--v1.png"
@@ -85,7 +85,7 @@ const MuseumCard = ({
               />
               </span>
             }{
-              !isPrivate && 
+              isPrivate && 
               <span className="Card-statusPrivate">
                 <img
                 src="https://img.icons8.com/ios/50/000000/private-lock.png"
